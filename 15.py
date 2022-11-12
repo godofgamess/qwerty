283e8c464b5f82a7a8ee91b42767b9da4d8d47d6
Ivanwg <81613195+Ivanwg@users.noreply.github.com>
2022-11-12 20:31:50 +0300

    15 changed

--- 15.py
+++ 15.py
@@ -1,16 +1,22 @@
+numbers_list = []
+
+
 while True:
     count_numbers = int(input('Введите количество чисел: '))
     if 1 <= count_numbers <= 1000:
         break
     else:
         print('Число должно быть в диопозоне от 1 до 1000. Повторите ввод. \n ')
-for num in range(1, count_numbers +1 ):
+
+for num in range(1, count_numbers + 1):
     while True:
-        numbers = int(input('Введите число: '))
-        if 1 <= numbers <= 30000:
+        number = int(input('Введите число: '))
+        if 1 <= number <= 30000:
+            if (number % 10 == 4):
+                numbers_list.append(number)
             break
         else:
-            print('Число должно быть в диопозоне от 1 до 30000. Повторите ввод. \n') #Похоже нужен список
+            print('Число должно быть в диопозоне от 1 до 30000. Повторите ввод. \n')
 
 
-print(min(numbers))
+print(min(numbers_list))
